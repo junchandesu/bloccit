5dcc62a54663764a5dd2182f00254742fce00390
Junko Werner <aliciajace@gmail.com>
2015-04-30 17:03:51 -0700

    modified seed.rf condition block print

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,34 @@
 require 'faker'
 
-50.times   do 
-	Post.create!(
-		title: Faker::Lorem.sentence,
-		body: Faker::Lorem.paragraph
-		)
-end
-posts = Post.all
+# #50.times   do 
+# # 	Post.create!(
+# # 		title: Faker::Lorem.sentence,
+# # 		body: Faker::Lorem.paragraph
+# # 		)
+# # #end
+# posts = Post.all
 
-100.times do 
-	Comment.create!(
-		post: posts.sample,
-		body: Faker::Lorem.paragraph
-		)
+# 1#00.times do 
+# 	Comment.create!(
+# 		post: posts.sample,
+# 		body: Faker::Lorem.paragraph
+# 		)
+# #end
+
+# puts "Seed finished"
+# puts "#{Post.count} posts created"
+# puts "#{Comment.count} comments crated"
+
+
+post1 = "Orange Banana"
+body1 = "Good morning. Good afternoon. Good night"
+
+if Post.select { |x| x.title != post1 } && Post.select { |y| y.body != body1}
+Post.create!(
+	title: post1,
+	body: body1
+	)
+puts "The new unique post title: '#{post1}'and body: '#{body1}' are added."
+puts "In posts table, #{Post.count} posts are in total"
 end
 
-puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments crated"
