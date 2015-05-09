3da9ff088b036cb408b1463452c849c51d01d924
Junko Werner <aliciajace@gmail.com>
2015-05-09 15:42:52 -0700

    updated markdown in Post and updated summary-new

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,22 +11,22 @@ class Post < ActiveRecord::Base
 	validates :user, presence: true
 
 	def markdown_title
-		render_as_markdown
-		(@redcarpet.render  title).html_safe
+		render_as_markdown(title)
+		
 	end
 
 	def markdown_body
-		render_as_markdown
-		(@redcarpet.render body).html_safe
+		render_as_markdown(body)
+		
 	end
 
 	private
 
-	def render_as_markdown
+	def render_as_markdown(text)
 		renderer= Redcarpet::Render::HTML.new
 		extensions = {fenced_code_blocks: true}
 		@redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-		
+		(@redcarpet.render  text).html_safe
 	end
 
 
