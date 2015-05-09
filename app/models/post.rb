1f95d206d3e2f847efc839f651f3254124e7a396
Junko Werner <aliciajace@gmail.com>
2015-05-09 11:53:39 -0700

    imcomplete markdown_title&body passing the object

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,27 @@ class Post < ActiveRecord::Base
 	validates :body, length: { minimum: 20 }, presence: true
 	validates :topic, presence: true
 	validates :user, presence: true
+
+	def markdown_title
+		render_as_markdown
+		(@redcarpet.render  title).html_safe
+	end
+
+	def markdown_body
+		render_as_markdown
+		(@redcarpet.render body).html_safe
+	end
+
+	private
+
+	def render_as_markdown
+		renderer= Redcarpet::Render::HTML.new
+		extensions = {fenced_code_blocks: true}
+		@redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+		
+	end
+
+
+
+
 end
