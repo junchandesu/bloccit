2a2f14129be8e4250094e3f34828bf8721dd63eb
Junko Werner <aliciajace@gmail.com>
2015-05-13 20:20:23 -0700

    done change to Image from Avatar uploader User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513040456) do
+ActiveRecord::Schema.define(version: 20150514025806) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -99,6 +99,8 @@ ActiveRecord::Schema.define(version: 20150513040456) do
     t.datetime "updated_at"
     t.string   "role"
     t.string   "avatar"
+    t.string   "pictire"
+    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
