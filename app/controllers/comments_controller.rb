abcf402450afe357815af8099eb6dd4467f39c25
Junko Werner <aliciajace@gmail.com>
2015-05-25 16:36:48 -0700

    completed comments separate

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,41 +3,41 @@ class CommentsController < ApplicationController
 
    
 def new
-    @topic = Topic.find(params[:topic_id])
+   # @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     @comment = Comment.new
   #  authorize @comment
  end
 
 def create
-   @topic = Topic.find(params[:topic_id])
+ #  @topic = Topic.find(params[:topic_id])
    @post = Post.find(params[:post_id])
    @comment = current_user.comments.build(params.require(:comment).permit(:body))
    @comment.post = @post
+   @new_comment = Comment.new
    authorize @comment
 
    if @comment.save
    	flash[:notice] = "Comment was saved"
-   	redirect_to [@topic, @post]
+   	redirect_to [@post.topic, @post]
    else
-   	flash[:error] = "Error creating comments. Please try again to enter at least 5 letters."
-   	redirect_to [@topic, @post]
+   	render :new
    end
 end
 
 
    def destroy
-     @topic = Topic.find(params[:topic_id])
-     @post = @topic.posts.find(params[:post_id])
+  #   @topic = Topic.find(params[:topic_id])
+     @post = posts.find(params[:post_id])
      @comment = @post.comments.find(params[:id])
  
      authorize @comment
      if @comment.destroy
        flash[:notice] = "Comment was removed."
-       redirect_to [@topic, @post]
+       redirect_to [@post.topic, @post]
      else
        flash[:error] = "Comment couldn't be deleted. Try again."
-       redirect_to [@topic, @post]
+        redirect_to [@post.topic, @post]
      end
    end
 
