0124ad3dba72b7711d700be991256e7600300c36
Junko Werner <aliciajace@gmail.com>
2015-06-06 09:01:42 -0700

    favorited method with user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,22 @@ describe User do
 	include TestFactories
 
 	describe "#favorited(post)" do
-		xit "returns 'nil' if the user has not favorited the post" do
-	    end
+		before do
+			@post = associated_post
+			@user = authenticated_user
+			
+		end
 
-		xit "Retrns the appropriate favorite if it exists" do
+	
+		it "returns 'nil' if the user has not favorited the post" do
+	   		expect(@user.favorited(@post)).to be_nil
+
+	   end
+
+		it "Retrns the appropriate favorite if it exists" do
+			favorite = @user.favorites.where(post_id: @post.id).first
+		expect( @user.favorited(@post)).to eq(favorite)
 		end
 	end
 end
+
