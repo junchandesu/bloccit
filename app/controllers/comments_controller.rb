58648b2f06d051a20e60ed5f4da129a14f85f812
Junko Werner <aliciajace@gmail.com>
2015-06-14 11:29:43 -0700

    Ajax Create prepend

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,15 +12,18 @@ def new
 def create
  #  @topic = Topic.find(params[:topic_id])
    @post = Post.find(params[:post_id])
-   @comment = current_user.comments.build(params.require(:comment).permit(:body))
+   @comment = @post.comments
+   @comment = current_user.comments.build(comment_params)
    @comment.post = @post
+
    @new_comment = Comment.new
    authorize @comment
 
    if @comment.save
     flash[:notice] = "Comment was saved"
-    redirect_to [@post.topic, @post]
+    #redirect_to [@post.topic, @post]
    else
+    flash[:error] = "There was an error saving the comment."
     render :new
    end
 end
@@ -45,5 +48,11 @@ end
        format.html
        format.js
      end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 
 end
